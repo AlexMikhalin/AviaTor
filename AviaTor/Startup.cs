ab7ce7e79ab680911e18869782ef5cf6b411c8ec
sashamikhalin2017 <sashamikhalin2017@yandex.ru>
2017-12-02 22:54:03 +0300

    create all

---
+++ AviaTor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AviaTor.Startup))]
+namespace AviaTor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
